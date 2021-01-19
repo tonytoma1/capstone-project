97a9358a3834e67fc39c5e954a11292dac4ed7b5
tonytoma1 <tony.toma2@outlook.com>
2021-01-19 14:53:28 -0700

    added RestController, Service, Model and Repository for the DeviceCondition table

---
+++ backend/src/main/java/com/ez/wireless/cellphone/capstone/model/DeviceCondition.java
@@ -0,0 +1,55 @@
+package com.ez.wireless.cellphone.capstone.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "device_condition")
+public class DeviceCondition {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "condition_sequence")
+	@SequenceGenerator(name="condition_sequence", sequenceName = "condition_sequence", allocationSize = 1)
+	@Column(name = "condition_id")
+	private Integer conditionId;
+	
+	@Column(name = "condition_name", unique = true)
+	private String conditionName;
+
+	/**
+	 * @return the conditionId
+	 */
+	public Integer getConditionId() {
+		return conditionId;
+	}
+
+	/**
+	 * @param conditionId the conditionId to set
+	 */
+	public void setConditionId(Integer conditionId) {
+		this.conditionId = conditionId;
+	}
+
+	/**
+	 * @return the conditonName
+	 */
+	public String getConditonName() {
+		return conditionName;
+	}
+
+	/**
+	 * @param conditonName the conditonName to set
+	 */
+	public void setConditionName(String conditionName) {
+		this.conditionName = conditionName;
+	}
+
+	
+	
+}
---
+++ backend/src/main/java/com/ez/wireless/cellphone/capstone/repository/DeviceConditionRepository.java
@@ -0,0 +1,11 @@
+package com.ez.wireless.cellphone.capstone.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ez.wireless.cellphone.capstone.model.DeviceCondition;
+
+@Repository
+public interface DeviceConditionRepository extends CrudRepository<DeviceCondition, Integer> {
+
+}
