ebc313d0a93e0bd16d1e7620b62f02af1bd5a68f
Dakota H. <dakotaharvey86@gmail.com>
2021-01-22 11:49:28 -0700

    fixed orders complete

--- backend/src/main/java/com/ez/wireless/cellphone/capstone/controller/OrdersCompleteController.java
+++ backend/src/main/java/com/ez/wireless/cellphone/capstone/controller/OrdersCompleteController.java
@@ -1,25 +1,36 @@
 package com.ez.wireless.cellphone.capstone.controller;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.ez.wireless.cellphone.capstone.model.OrdersComplete;
+import com.ez.wireless.cellphone.capstone.service.OrdersCompleteService;
+
+@RestController
+@RequestMapping("/api/orders-complete")
 public class OrdersCompleteController 
 {
-
-	private OrdersCompleteService ordersCompleteService
+	@Autowired
+	private OrdersCompleteService ordersCompleteService;
 	
 	@GetMapping
-	public List<ClientOrders> getAll() 
+	public List<OrdersComplete> getAllOrdersComplete() 
 	{
-		return ordersCompleteService.getAllClientOrders();
+		return ordersCompleteService.getAllOrdersComplete();
 	}
 	
 	@PostMapping
-	publi void saveOrdersComplete(@RequestBody OrdersComplete ordersComplete)
-	try {
-		ordersCompleteService(ordersComplete);
+	public void saveOrdersComplete(@RequestBody OrdersComplete ordersComplete) {
+		try {
+			ordersCompleteService.saveOrdersComplete(ordersComplete);
+		}
+		catch(IllegalArgumentException e) {
+			throw new IllegalArgumentException("Order cannot be saved or processed.");
+		}
 	}
-	catch(IllegalArgumentException e) {
-		throw new IllegalArgumentException("Order cannot be saved or processed.");
-	}
-	
 }
--- backend/src/main/java/com/ez/wireless/cellphone/capstone/model/OrdersComplete.java
+++ backend/src/main/java/com/ez/wireless/cellphone/capstone/model/OrdersComplete.java
@@ -1,6 +1,12 @@
-package com.ez.wireless.cellphone.capstone.controller;
+package com.ez.wireless.cellphone.capstone.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "orders_complete")
