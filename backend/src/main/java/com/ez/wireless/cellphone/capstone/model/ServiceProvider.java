4ac4aec5bc98f0fa2a4e1d7911b0d2c286cb5fd0
tonytoma1 <tony.toma2@outlook.com>
2021-01-21 21:30:44 -0700

    Added ServiceProvider model

---
+++ backend/src/main/java/com/ez/wireless/cellphone/capstone/model/ServiceProvider.java
@@ -0,0 +1,53 @@
+package com.ez.wireless.cellphone.capstone.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "service_provider")
+public class ServiceProvider {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "service_provider_sequence")
+    @SequenceGenerator(name="service_provider_sequence", sequenceName = "service_provider_sequence", allocationSize = 1)
+	@Column(name = "service_provider_id")
+	private Integer serviceProviderId;
+	
+	@Column(name = "service_provider_name")
+	private String serviceProviderName;
+
+	/**
+	 * @return the serviceProviderId
+	 */
+	public Integer getServiceProviderId() {
+		return serviceProviderId;
+	}
+
+	/**
+	 * @param serviceProviderId the serviceProviderId to set
+	 */
+	public void setServiceProviderId(Integer serviceProviderId) {
+		this.serviceProviderId = serviceProviderId;
+	}
+
+	/**
+	 * @return the serviceProviderName
+	 */
+	public String getServiceProviderName() {
+		return serviceProviderName;
+	}
+
+	/**
+	 * @param serviceProviderName the serviceProviderName to set
+	 */
+	public void setServiceProviderName(String serviceProviderName) {
+		this.serviceProviderName = serviceProviderName;
+	}
+	
+	
+}
