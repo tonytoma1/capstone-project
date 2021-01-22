1ec964fc397f0aa697e288393e3de4216d01a5e9
tonytoma1 <tony.toma2@outlook.com>
2021-01-22 11:37:55 -0700

    fixed changes

--- backend/src/main/java/com/ez/wireless/cellphone/capstone/model/Account.java
+++ backend/src/main/java/com/ez/wireless/cellphone/capstone/model/Account.java
@@ -1,11 +1,75 @@
 package com.ez.wireless.cellphone.capstone.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "account")
 public class Account {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "account_sequence")
+	@SequenceGenerator(name="account_sequence", sequenceName = "account_sequence", allocationSize = 1)
+	@Column(name = "account_id")
+	private Integer accountId;
+	
+	@Column(name = "username")
+	private String username;
+	
+	@Column(name = "password")
+	private String password;
+
+	// TODO add person object
+	
+	
+	/**
+	 * @return the accountId
+	 */
+	public Integer getAccountId() {
+		return accountId;
+	}
+
+	/**
+	 * @param accountId the accountId to set
+	 */
+	public void setAccountId(Integer accountId) {
+		this.accountId = accountId;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
 	
 }
---
+++ backend/src/main/java/com/ez/wireless/cellphone/capstone/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.ez.wireless.cellphone.capstone.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ez.wireless.cellphone.capstone.model.Account;
+
+@Repository
+public interface AccountRepository extends CrudRepository<Account, Integer> {
+
+}
