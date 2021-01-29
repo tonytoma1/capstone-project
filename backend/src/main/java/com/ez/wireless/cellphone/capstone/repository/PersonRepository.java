9ca549e4162de8b2d6e0f11d36c3b1f5cd5207ee
tonytoma1 <tony.toma2@outlook.com>
2021-01-28 20:59:22 -0700

    Created Person model, repository, service and controller.

---
+++ backend/src/main/java/com/ez/wireless/cellphone/capstone/repository/PersonRepository.java
@@ -0,0 +1,11 @@
+package com.ez.wireless.cellphone.capstone.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ez.wireless.cellphone.capstone.model.Person;
+
+@Repository
+public interface PersonRepository extends CrudRepository<Person, Integer> {
+
+}
---
+++ backend/src/main/java/com/ez/wireless/cellphone/capstone/service/AccountService.java
@@ -0,0 +1,9 @@
+package com.ez.wireless.cellphone.capstone.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+	
+}
