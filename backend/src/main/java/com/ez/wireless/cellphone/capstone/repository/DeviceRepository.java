d4bf0b4b2944ec7a66d442ede999bc81b41cfc84
Dakota H. <dakotaharvey86@gmail.com>
2021-02-06 12:12:05 -0700

    Fixed @service and @repository

--- backend/src/main/java/com/ez/wireless/cellphone/capstone/repository/DeviceRepository.java
+++ backend/src/main/java/com/ez/wireless/cellphone/capstone/repository/DeviceRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.ez.wireless.cellphone.capstone.model.Device;
 
+@Repository
 public interface DeviceRepository extends CrudRepository<Device, Integer>{
 
 }
