cb419d44e276d6d5813f12d2c054ff00b97c17b0
Dakota H. <dakotaharvey86@gmail.com>
2021-02-08 10:54:29 -0700

    started on the junit test for the shippo api

---
+++ backend/src/test/java/com/ez/wireless/cellphone/capstone/ShippoTest.java
@@ -0,0 +1,33 @@
+package com.ez.wireless.cellphone.capstone;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.shippo.model.Shipment;
+
+class ShippoTest 
+{	
+	private Shipment ship;
+	
+	@BeforeEach
+	void setUp() throws Exception
+	{
+		this.ship = new Shipment();
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception
+	{
+		
+	}
+	
+	@Test
+	void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
