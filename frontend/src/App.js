78758b424e5ea290c0886293205d106ffe074fc0
tonytoma1 <tony.toma2@outlook.com>
2021-02-11 21:18:31 -0700

    added bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {Footer} from './components/common'
+import Footer from './components/common/footer'
+import Home from './pages/home'
 
 function App() {
   return (
     <div className="App">
+    
      <Footer />
+     <Home/>
     </div>
   );
 }
---
+++ frontend/src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Card, CardBody} from 'reactstrap';
+
+export default class Home extends React.Component {
+
+
+    render() {
+        return(
+            <div>
+                <Card>
+                    <CardBody></CardBody>
+                </Card>
+            </div>
+        );
+    }
+}
