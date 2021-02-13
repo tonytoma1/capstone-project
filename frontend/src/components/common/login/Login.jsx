9fbac80aee49662f1f0374b131554370e0b8657f
tonytoma1 <tony.toma2@outlook.com>
2021-02-13 13:05:02 -0700

    Changed Login component to a class in order to handle password and email input

--- frontend/src/components/common/login/Login.jsx
+++ frontend/src/components/common/login/Login.jsx
@@ -2,62 +2,63 @@ import React from 'react';
 import Logo from '../../../images/logo.png';
 import './login-css.css';
 
-function Login () {
 
-  return (
-    
-    <div className="login-container">
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    // Changes the email state every time the email input is modified.
+    handleEmailChange(event) {
+        this.setState({email: event.target.value});
+    }
 
 
-        <figure>
-            <img src= {Logo} alt="Recommerce" />
-            <figcaption>Recommerce</figcaption>
-        </figure>
+    // Changes the password state every time the input is modified
+    handlePasswordChange(event) {
+        this.setState({password: event.target.value});
+    }
 
-       
-        <form>
-            <section>
-                <p>Email</p>
-                <input type="email" id="loginEmail" placeholder="Enter your Email here"/>
-            </section>
+        render() {
+            return(
+                <div className="login-container">
+                    <figure>
+                        <img src= {Logo} alt="Recommerce" />
+                        <figcaption>Recommerce</figcaption>
+                    </figure>
 
-            <section>
-            <p>Password</p>
-            <input type="Password" id="loginPassword" placeholder="Enter your Password here"/>
-            </section>
-            <input type="Submit" class="submitButton" id="loginSignIn" value="SignIn" />
-        </form>
-        
-        <article> 
-            Forgot password? Click 
-            <p>&nbsp;here.</p>
-        </article>
-    </div>
-    
-  )
+                    <form>
+                        <section>
+                            <p>Email</p>
+                            <input type="email" id="loginEmail" value={this.state.email}
+                                                                onChange={this.handleEmailChange} 
+                                                                placeholder="Enter your Email here"/>
+                        </section>
 
-}
+                        <section>
+                            <p>Password</p>
+                            <input type="Password" id="loginPassword" value={this.state.password} 
+                                                                      onChange={this.handlePasswordChange}
+                                                                     placeholder="Enter your Password here"/>
+                        </section>
+                        <input type="Submit" class="submitButton" id="loginSignIn" value="SignIn" />
+                    </form>
+                    
+                    <article> 
+                        Forgot password? Click 
+                        <p>&nbsp;here.</p>
+                    </article>
+                </div>
+            );
+        }
+    }
 
-export default Login;
 
-{/* FORM PREVIOUS CODE
-
-<figure>
-            <img src= {Logo} alt="Recommerce" />
-            <figcaption>Recommerce</figcaption>
-        </figure>
-
-        <section>
-            <p>Email</p>
-            <input type="email" id="loginEmail" placeholder="Enter your Email here"/>
-        </section>
-
-        
-        <section>
-            <p>Password</p>
-            <input type="Password" id="loginPassword" placeholder="Enter your Password here"/>
-        </section>
-
-        <button id="loginSignIn" value="SignIn"> Sign in </button>
-
- */}
