ebe7f489321a81d46e9b178b8917b797bc1f6a6f
Dakota H. <dakotaharvey86@gmail.com>
2021-03-02 11:23:52 -0700

    Started creating the confirm order page
    Haven't tested it out yet

---
+++ frontend/src/pages/confirm-order.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../components/common/footer/Footer'
+import Header_LP from '../components/common/Header/Header_LP'
+import confirmOrder from '../components/common/order/confirm-order'
+
+function confirmOrder() {
+  return (
+    <div className="App">
+    <Header_LP />
+	<confirmOrder />
+    <Footer />
+    </div>
+  );
+}
+
+export default confirmOrder;
