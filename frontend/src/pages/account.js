77ceda186168082caeea25203a82ee34dcb2c749
tonytoma1 <tony.toma2@outlook.com>
2021-03-03 19:03:12 -0700

    modified login

--- frontend/src/pages/account.js
+++ frontend/src/pages/account.js
@@ -4,6 +4,7 @@ import  { Redirect } from 'react-router-dom'
 import Header from 'components/common/Header/Header_LP';
 import Footer from 'components/common/footer/Footer';
 import Cookies from 'js-cookie';
+import history from '../history';
 
 export default class Account extends React.Component {
     constructor(props) {
@@ -18,30 +19,32 @@ export default class Account extends React.Component {
 
     componentDidMount() {
         // check to see if the user is logged in
-        UserService.isUserLoggedIn()
-                    .then((response) => {
-                        this.setState({userFound: true, user: response});
-                    })
-                    .catch((error) => {
-                        this.setState({userFound: false, user: null});
-                    })
+        try {
+            UserService.isUserLoggedIn()
+            .then((response) => {
+                this.setState({userFound: true, user: response});
+            })
+            .catch((error) => {
+                this.setState({userFound: false, user: null});
+
+                console.log(error);
+               history.push("/login");
+            });
+        }
+        catch(e) {
+            console.log(e);
+        }
+        
     }
 
     render() {
         
-        const {userFound} = this.state;
-
-        if(!userFound) {
-            {/* User isn't found. Send them back to the login screen */}
-            return <Redirect to="/login" />
-        }
-        else {
             return(<div>
                 <Header/>
                 <h1>Account page </h1>
                 <Footer/>
                 </div>   
             );
-        }
+    
     }
 }
