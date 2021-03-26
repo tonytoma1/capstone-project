d7e60b38295b6bf57903088518753cfab0e82643
Dorgee <dorgee.lama@edu.sait.ca>
2021-03-25 23:08:56 -0600

    commit

--- frontend/src/services/adminservice.js
+++ frontend/src/services/adminservice.js
@@ -4,7 +4,7 @@ import * as Constants from 'constants/global-constants';
 import jwt_decode from "jwt-decode";
 
 const apiUrl= 'http://localhost:8080/api/person';
-
+const apiUrl2= 'http://localhost:8080/api/new-orders';
 class AdminService{
 
 
@@ -15,6 +15,15 @@ async verifyAdmin() {
         headers: jsonResult
     }
     return await axios.get(apiUrl, config);
+}
+
+async viewOrder() {
+    const token = Cookies.get(Constants.JWT_TOKEN);
+    var jsonResult = {Authorization: 'Bearer '.concat(token)};
+    var config = {
+        headers: jsonResult
+    }
+    return await axios.get(apiUrl2, config);
 }
 
 deleteEmployee(personId){
