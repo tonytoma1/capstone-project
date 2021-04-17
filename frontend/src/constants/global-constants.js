09d00a61bfdb0cd9f6afbe61fc8d5c989adfb6d3
tonytoma1 <tony.toma2@outlook.com>
2021-04-17 12:09:25 -0600

    change api url

--- frontend/src/constants/global-constants.js
+++ frontend/src/constants/global-constants.js
@@ -1,3 +1,3 @@
 export const JWT_TOKEN = 'jwtToken';
-//export const API_URL = " https://recommercestore.herokuapp.com/api/";
-export const API_URL = " http://localhost:8080/api/";
+export const API_URL = " https://recommercestore.herokuapp.com/api/";
+//export const API_URL = " http://localhost:8080/api/";
